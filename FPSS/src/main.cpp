ad2d3a0e88ff87176b0f6b8c500ef3996e0a859b
SeveralCube22 <manam.viswadeep@gmail.com>
2022-05-09 19:41:29 -0700

    Removed project dependencies

--- FPSS/src/main.cpp
+++ FPSS/src/main.cpp
@@ -10,8 +10,6 @@
 #include <glm\gtc\type_ptr.hpp>
 #include <assimp\Importer.hpp>
 
-#include <CGAL/Simple_cartesian.h>
-
 #include "buffer.hpp"
 #include "buffer_layout.hpp"
 #include "vertex_array.hpp"
@@ -58,20 +56,6 @@ std::vector<glm::mat4> setupBuildingModels(); // setup building model matrices
 
 int main(void)
 {
-    typedef CGAL::Simple_cartesian<double> Kernel;
-    typedef Kernel::Point_2 Point_2;
-    typedef Kernel::Segment_2 Segment_2;
-
-    Point_2 p(1, 1), q(10, 10);
-    std::cout << "p = " << p << std::endl;
-    std::cout << "q = " << q.x() << " " << q.y() << std::endl;
-    std::cout << "sqdist(p,q) = "
-        << CGAL::squared_distance(p, q) << std::endl;
-    Segment_2 s(p, q);
-    Point_2 m(5, 9);
-    std::cout << "m = " << m << std::endl;
-    std::cout << "sqdist(Segment_2(p,q), m) = "
-        << CGAL::squared_distance(s, m) << std::endl;
     // Configurations
 
     /* Initialize the library */
