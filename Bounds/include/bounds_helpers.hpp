4a862bf5862946605480077f0c53c19f98d7eec2
SeveralCube22 <manam.viswadeep@gmail.com>
2022-07-23 00:32:06 -0400

    Additional clean up

--- Bounds/include/bounds_helpers.hpp
+++ Bounds/include/bounds_helpers.hpp
@@ -2,8 +2,6 @@
 
 #include "aabb.hpp"
 #include "frustum.hpp"
-#include "plane.hpp"
 
 bool aabbIntersectsAABB(const AABB& o1, const AABB& o2);
-bool aabbIntersectsPlane(const AABB& o1, const Plane& o2);
 bool aabbIntersectsFrustum(const AABB& o1, const Frustum& o2);
--- Bounds/src/bounds_helpers.cpp
+++ Bounds/src/bounds_helpers.cpp
@@ -4,16 +4,6 @@ bool aabbIntersectsAABB(const AABB& o1, const AABB& o2) {
 	return o1.contains(o2.getMin()) && o1.contains(o2.getMax());
 }
 
-bool aabbIntersectsPlane(const AABB& o1, const Plane& o2) {
-	const glm::vec3& extents = o1.getExtents();
-	float r = extents.x * std::abs(o2.getNormal().x) + extents.y * std::abs(o2.getNormal().y) + extents.z * std::abs(o2.getNormal().z);
-	
-	return std::abs(o2.getDistanceFromPlane(o1.getCenter())) <= r;
-}
-
 bool aabbIntersectsFrustum(const AABB& o1, const Frustum& o2) {
-	for (const Plane& plane : o2.getPlanes())
-		if (aabbIntersectsPlane(o1, plane))
-			return true;
-	return false;
+	return o2.IsBoxVisible(o1.getMin(), o1.getMax());
 }
